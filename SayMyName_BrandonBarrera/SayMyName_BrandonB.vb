00023a138e889e58017952e9fc55ee8d966c0a60
Barkly-Brandon <bubba05barrera@gmail.com>
2025-01-23 16:25:24 -0700

    Add project files.

---
+++ SayMyName_BrandonBarrera/SayMyName_BrandonB.vb
@@ -0,0 +1,23 @@
+﻿'Brandon Barrera
+'Spring 2025
+'RCET2265
+'SayMyName
+'
+Module SayMyName_BrandonB
+
+    Sub Main()
+        Dim Name As String 'Set aside RAM for variable
+
+        Console.WriteLine("What is your name?") 'Ask the user their name
+        Name = Console.ReadLine 'Set their name as a variable
+        Console.WriteLine()
+
+        If (Name = "Danger") Or (Name = "Walter") Or (Name = "danger") Or (Name = "walter") Then
+            Console.WriteLine("Hello Mr.White")
+        ElseIf (Name = Name) Then
+            Console.WriteLine("Your Name is " & Name & "!") 'Say their name
+        End If
+
+    End Sub
+
+End Module
